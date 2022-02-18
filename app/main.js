671711336b147a27527376a215503396f4b7ae05
pkol92 <paulina_kolasa@wp.pl>
2022-02-18 21:39:43 +0100

    add interact code

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import {SmartDevice} from "../app/devices/smartDevices.js";
 import {SmartBulb} from "../app/devices/bulb.js";
+import interact from 'https://cdn.interactjs.io/v1.10.11/interactjs/index.js'
 
 const divPanel = document.getElementById("deviceList");
 const listDevices = document.createElement("ul");
@@ -37,6 +38,28 @@ const details = (device) => {
 
 
 details(SmartBulb);
+
+
+const position = { x: 0, y: 0 }
+
+interact('.deviceView').draggable({
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: 'parent',
+      endOnly: true
+    })
+  ],
+  listeners: {
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
+
 
 // const url = '/api/v1/devices';
 
